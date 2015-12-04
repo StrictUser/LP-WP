33c871b1bd40ecfe107b621832994c70767346de
StrictUser <ermolenko198528@gmail.com>
2015-12-05 01:44:56 +0200

    Add some changes to common.js and main.sass. Finish the project!

--- __clean_html_original/js/common.js
+++ __clean_html_original/js/common.js
@@ -7,20 +7,17 @@ $(document).ready(function() {
         $(this).addClass("active");
     });
 
-    $(".top_text h1").animated("fadeInDown", "fadeOutUp");
-    $(".top_text p, .section_header").animated("fadeInUp", "fadeOutDown");
-
     $(".animation_1").animated("flipInY", "flipOutY");
     $(".animation_2").animated("fadeInLeft", "fadeOutLeft");
     $(".animation_3").animated("fadeInRight", "fadeOutRight");
 
-    $(".left .resume_item").animated("fadeInLeft", "fadeOutLeft");
-    $(".right .resume_item").animated("fadeInRight", "fadeOutRight");
+    $(".left .resume_item").animated("fadeInLeft", "fadeOutDown");
+    $(".right .resume_item").animated("fadeInRight", "fadeOutDown");
 
     $(".popup").magnificPopup({type:'image'});
     $(".popup_content").magnificPopup({type:'inline', midClick: true, closeOnContentClick: true});
 
-    $("section_")
+    $("section_header").animated("fadeInUp", "fadeOutDown");
 
     function heightDetect(){
         $(".main_head").css("height", $(window).height());
@@ -41,15 +38,16 @@ $(document).ready(function() {
     $(".top_mnu ul a").click(function(){
         $(".top_mnu").fadeOut(400);
         $("#sandwich").toggleClass("active");
+        $(".top_text").css("opacity", "1");
     }).append("<span>");
 
     $(".toggle_mnu").click(function(){
         if($(".top_mnu").is(":visible")){
-            $(".top_text").removeClass("h_opacify");
+            $(".top_text").css("opacity", "1");
             $(".top_mnu").fadeOut(400);
             $(".top_mnu li a").removeClass("fadeInUp animated");
         }else{
-            $(".top_text").addClass("h_opacify");
+            $(".top_text").addClass("opacity", ".2");
             $(".top_mnu").fadeIn(600);
             $(".top_mnu li a").addClass("fadeInUp animated");
         }
@@ -60,11 +58,14 @@ $(document).ready(function() {
         $(this).find(".port_descr").attr("id", "work_" + i);
     });
 
-    $("input,select,textarea").not("[type=submit]").jqBootstrapValidation();
+    $("input,select,textarea").jqBootstrapValidation();
 
 });
 
 $(window).load(function() {
     $(".loader_inner").fadeOut();
     $(".loader").delay(400).fadeOut("slow");
+
+    $(".top_text h1").animated("fadeInDown", "fadeOutUp");
+    $(".top_text p").animated("fadeInUp", "fadeOutDown");
 });
